5ddb310cecb4ad78f7796e2aa57fe0345849777e
John Lee <PiggySpeed@users.noreply.github.com>
2018-01-13 14:29:30 -0800

    added banner

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import Timer from '../banners/Timer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Timer />
+        {/* Every 20-30 seconds, switch between <Metronome /> and <Timer /> components */}
       </div>
     );
   }
