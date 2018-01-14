ea4d3ef1acb9ba11e113368800e46c7dea48a7cf
jacob.s <jacob.shieh@gmail.com>
2018-01-14 02:49:15 -0800

    stdlib functions now update boolean values

---
+++ client/stdlib-workspace/ahong1/louder/functions/__main__.js
@@ -0,0 +1,88 @@
+/**
+* A basic Hello World function
+* @param {string} name Who you're saying hello to
+* @returns {any}
+*/
+module.exports = (name = 'world', context, callback) => {
+	var AWS = require("aws-sdk");
+	AWS.config.update({
+			accessKeyId: 'AKIAIIZPL74DK4BXD45Q',
+			secretAccessKey: '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',
+			region: "us-west-2"
+	});
+var DB = new AWS.DynamoDB();
+var threshold;
+var louds;
+
+DB.getItem({
+		TableName: "nwHackDemo",
+		Key: {
+				data: {
+						S: "Data"
+				}
+		}
+}, function(err, data) {
+
+		console.log(data.Item.numPeople.N)
+		threshold = parseInt(data.Item.numPeople.N) / 5;
+		louds = parseInt(data.Item.louder.N) + 1;
+
+		var params = {
+				ExpressionAttributeNames: {
+						"#NP": "louder"
+				},
+				ExpressionAttributeValues: {
+						":t": {
+								N: String(louds)
+						}
+				},
+
+
+				ReturnValues: "ALL_NEW",
+				TableName: "nwHackDemo",
+				UpdateExpression: "SET #NP = :t ",
+				Key: {
+						data: {
+								S: "Data"
+						}
+				}
+		}
+
+
+		DB.updateItem(params, function(err,data){
+				if (err) console.log(err, err.stack); // an error occurred
+				// else     console.log(data);
+
+				if (louds >= threshold) {
+
+					var params = {
+						ExpressionAttributeNames: {
+								"#NP": "isLouder"
+						},
+						ExpressionAttributeValues: {
+								":t": {
+										BOOL: true
+								}
+						},
+	
+						ReturnValues: "ALL_NEW",
+						TableName: "nwHackDemo",
+						UpdateExpression: "SET #NP = :t ",
+						Key: {
+								data: {
+										S: "Data"
+								}
+						}
+				}
+					DB.updateItem(params, function(err,data){
+						if (err) console.log(err, err.stack); // an error occurred
+						// else     console.log(data);
+
+						callback(null, data);
+					});
+				}
+
+				else callback(null, data);		})
+
+})
+};
