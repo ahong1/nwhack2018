c544783a63cec4f647c656b6f97418eeb9f4bfbe
Alfred H <alfred.hong@twotalltotems.com>
2018-01-13 16:54:44 -0800

    moving comps

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import './App.css';
+import AWS from "aws-sdk";
+import VideoStreamContainer from "./VideoStreamContainer";
+
+AWS.config.update({
+    accessKeyId: "AKIAIYZ35V6DRF6WX4BA",
+    secretAccessKey: "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",
+    region: "us-west-2"
+})
 
 class App extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            Kinesis: new AWS.Kinesis()
+        }
+
+    }
+
+
+
   render() {
     return (
       <div className="App">
@@ -13,6 +31,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <VideoStreamContainer kinesis={this.state.Kinesis}/>
+
       </div>
     );
   }
