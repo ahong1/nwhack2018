001a602e89a014da42ea5c793168c5eadfc33160
ahong1 <alfred.hong@twotalltotems.com>
2018-01-14 09:23:31 -0800

    adding modal

--- client/src/containers/DashboardModal.js
+++ client/src/containers/DashboardModal.js
@@ -16,39 +16,46 @@ class DashboardModal extends Component {
     constructor(props) {
         super();
 
-        this.handleHide = this.handleHide.bind(this);
         this.state = {
            showModal: false
         };
 
 
     }
-    //
-    // componentDidMount() {
-    //     let header = new Headers({'Content-Type': 'application/json'});
-    //
-    //     let fetchArgs = {
-    //         method: 'POST',
-    //         body: JSON.stringify({ name: 'johnny' }),
-    //         headers: header,
-    //         mode: 'cors',
-    //         cache: 'default'
-    //     };
-    //
-    //     fetch("https://ahong1.lib.id/checkStats@dev/", fetchArgs).then(res => res.json())
-    //         .then(res => console.log('result is', res))
-    //         .catch(err => console.warn(err));
-    // }
-    //
-    // handleClose() {
-    //     this.setState ({
-    //         showModal: false
-    //     })
-    // }
+
+    componentDidMount() {
+        let header = new Headers({'Content-Type': 'application/json'});
+
+        let fetchArgs = {
+            method: 'POST',
+            body: JSON.stringify({ name: 'johnny' }),
+            headers: header,
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch("https://ahong1.lib.id/checkStats@dev/", fetchArgs).then(res => res.json())
+            .then(res => console.log('result is', res))
+            .catch(err => console.warn(err));
 
 
 
+        Myo.on('fist', function(){
+            console.log('Hello Myo!');
+            Myo.myos[0].vibrate('short');
+            Myo.myos[0].vibrate('short');
 
+            this.setState({
+                showModal: !this.state.showModal
+            })
+        });
+    }
+
+    handleClose() {
+        this.setState ({
+            showModal: false
+        })
+    }
 
     render() {
         return (
